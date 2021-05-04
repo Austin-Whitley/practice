9f800f16996b58d89497600b55f84a74ed63cd0e
Austin Whitley <whitley.austin01@gmail.com>
2021-05-04 09:16:10 -0500

    added a render inventory function

---
+++ js/inventory.js
@@ -0,0 +1,26 @@
+"use strict";
+function renderItem(item){
+    var html = '<div class="inventory-container d-flex>';
+    html += '<div class=""'
+}
+// function renderCoffee(coffee) {
+//     var html = '<div class="container d-flex coffee row col-4 cards grow" id="boxes">';
+//     //html += '<div>' + coffee.id + '</div>';
+//     html += '<div class="" id="coffeeName">' + coffee.name + '</div>';
+//     html += '<div class="" id="roastName">' + coffee.roast + '</div>';
+//     html += '</div>';
+//     return html;
+// }
+
+var inventory = [
+    {slot: 1},
+    {slot: 2},
+    {slot: 3},
+    {slot: 4},
+    {slot: 5},
+    {slot: 6},
+    {slot: 7},
+    {slot: 8},
+    {slot: 9},
+    {slot: 10},
+];
